23f3979e87218fe3da9d21e0956fcfffcdb691ea
hee <jkgt0521@naver.com>
2022-12-26 03:33:44 +0900

    feat: map fragment api

--- app/src/main/java/ddwu/mobile/finalproject/ma01_20200989/activities/HospitalActivity.java
+++ app/src/main/java/ddwu/mobile/finalproject/ma01_20200989/activities/HospitalActivity.java
@@ -1,21 +1,47 @@
 package ddwu.mobile.finalproject.ma01_20200989.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import ddwu.mobile.finalproject.ma01_20200989.R;
 
 public class HospitalActivity extends AppCompatActivity {
+    private final String CURRENT_MARKER_TITLE = "현재 위치";
+    private final int REQ_PERMISSION_CODE = 100;
+
     private GoogleMap googleMapObject;
+    private Location lastLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private Marker currentMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hospital);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(mapReadyCallback);
@@ -25,6 +51,66 @@ public class HospitalActivity extends AppCompatActivity {
         @Override
         public void onMapReady(@NonNull GoogleMap googleMap) {
             googleMapObject = googleMap;
+
         }
     };
+
+    private LocationRequest getLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            for (Location location : locationResult.getLocations()) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                lastLocation = location;
+                LatLng currentLocation = new LatLng(latitude, longitude);
+                googleMapObject.animateCamera(CameraUpdateFactory.newLatLng(currentLocation));
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(currentLocation);
+                markerOptions.title(CURRENT_MARKER_TITLE);
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+                currentMarker = googleMapObject.addMarker(markerOptions);
+                currentMarker.showInfoWindow();
+            }
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_PERMISSION_CODE:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "위치권한 획득 완료", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "위치권한 미획득", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void checkPermission() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,"Permissions Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, REQ_PERMISSION_CODE);
+        }
+    }
 }
