ae5631e48ea879611d41fee3369f1972177113ef
hee <jkgt0521@naver.com>
2022-12-25 20:20:51 +0900

    feat: project setting

---
+++ app/src/main/java/ddwu/mobile/finalproject/ma01_20200989/MainActivity.java
@@ -0,0 +1,14 @@
+package ddwu.mobile.finalproject.ma01_20200989;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
